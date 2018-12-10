64039e0c96c40168b108d77bcefc2ac7910af45f
Aseel <aseel@Yaseens-MacBook-Air.local>
2018-12-09 19:43:00 -0500

    pass ings and transform

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -2,16 +2,40 @@ import * as React from "react";
 import classes from "./Burger.scss";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
+export interface IBurgerProps {
+    ingredients: {
+        salad?: number;
+        bacon?: number;
+        cheese?: number;
+        meat?: number;
+    };
+}
 
-const burger = () =>{
-    return(
+const burger = (props: IBurgerProps) => {
+    const transformedIngredients = Object.keys(props.ingredients).reduce(
+        (accumulator, currentIngredient) => {
+            const amount = props.ingredients[currentIngredient];
+            const burgerIngredients = [];
+            for (let i = 0; i < amount; i++) {
+                burgerIngredients.push(
+                    <BurgerIngredient
+                        type={currentIngredient}
+                        key={currentIngredient + i}
+                    />
+                );
+            }
+            return [...accumulator, ...burgerIngredients];
+        },
+        []
+    );
+
+    return (
         <div className={classes.Burger}>
-            <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
-            <BurgerIngredient type="bread-bottom"/>
+            <BurgerIngredient type="bread-top" />
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom" />
         </div>
     );
-}
+};
 
 export default burger;
